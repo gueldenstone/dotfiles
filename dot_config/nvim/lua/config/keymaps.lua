da129328617c6b988a9dfce23f091c43b310a7c6
Lukas Güldenstein <git@lukasgueldenstein.de>
2024-01-20 21:26:21 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/plugins/colorscheme.lua Add .config/nvim/lua/plugins/notify.lua Add .config/nvim/lua/plugins/visual-multi.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -17,9 +17,3 @@ vim.api.nvim_set_keymap("", "<Right>", "<Nop>", { noremap = true, silent = true 
 
 -- keepcursor in the middle
 vim.opt.scrolloff = 30
-
--- Move to window using the <ctrl> hjkl keys
--- map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", noremap = true })
--- map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", noremap = true })
--- map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", noremap = true })
--- map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", noremap = true })
