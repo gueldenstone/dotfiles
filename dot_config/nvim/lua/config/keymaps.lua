b1f72b20e149f8c37bc9a632b040f2ec606cc128
Lukas Güldenstein <lukas.gueldenstein@dbaudio.com>
2024-04-14 10:24:16 +0200

    fix vim kitty navigation and kitty font

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -15,5 +15,23 @@ vim.api.nvim_set_keymap("i", "<Right>", "<Nop>", { noremap = true, silent = true
 vim.api.nvim_set_keymap("", "<Left>", "<Nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("", "<Right>", "<Nop>", { noremap = true, silent = true })
 
+-- Map KittyNavigateLeft to Ctrl+h
+vim.api.nvim_set_keymap("n", "<C-h>", ":KittyNavigateLeft<CR>", { noremap = true, silent = true })
+
+-- Map KittyNavigateDown to Ctrl+j
+vim.api.nvim_set_keymap("n", "<C-j>", ":KittyNavigateDown<CR>", { noremap = true, silent = true })
+
+-- Map KittyNavigateUp to Ctrl+k
+vim.api.nvim_set_keymap("n", "<C-k>", ":KittyNavigateUp<CR>", { noremap = true, silent = true })
+
+-- Map KittyNavigateRight to Ctrl+l
+vim.api.nvim_set_keymap("n", "<C-l>", ":KittyNavigateRight<CR>", { noremap = true, silent = true })
+
 -- keepcursor in the middle
 vim.opt.scrolloff = 30
+
+-- Remap CMD+B to CMakeBuild in normal mode
+vim.api.nvim_set_keymap("n", "<D-b>", ":CMakeBuild<CR>", { noremap = true, silent = true })
+
+-- Remap CMD+R to CMakeRun in normal mode
+vim.api.nvim_set_keymap("n", "<D-r>", ":CMakeRun<CR>", { noremap = true, silent = true })
